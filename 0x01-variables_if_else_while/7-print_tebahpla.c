41573ee405805e6f39d32bcc4d0d91ed57b905ba
Davee <davidossoh@gmail.com>
2022-10-14 13:30:51 -0700

    Devops

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - entry block
+ * @void: no argument
+ * Return: 0
+ **/
+
+int main(void)
+
+{
+		char c;
+
+		for (c = 'z' ; c >= 'a'; c--)
+		putchar(c);
+		putchar('\n');
+		return (0);
+
+}
