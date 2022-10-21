7837e4d1194fe834d157775e7a9ad0be8a5c389c
Davee <davidossoh@gmail.com>
2022-10-20 17:29:40 -0700

    Devops

---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,13 @@
+#include <unitsd.h>
+#include "main.h"
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: the character to print
+ * Return: On succes 1, else -1
+ **/
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
