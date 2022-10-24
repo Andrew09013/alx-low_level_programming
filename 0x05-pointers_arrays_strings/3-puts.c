98eb72e4e1d4773bca3740b82d7c1b9df24163b8
Davee <davidossoh@gmail.com>
2022-10-24 10:36:14 -0700

    Devops

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include"main.h"
+
+/**
+ * puts - prints a string, followed by a newline to stdout
+ * @str: string parameter to print
+ */
+
+void _puts(char *str)
+{
+	while (*str != '\0')
+		_putchar(*str++);
+	
+
+	_putchar('\n');
+}
