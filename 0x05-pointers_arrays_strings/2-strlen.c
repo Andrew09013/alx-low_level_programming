8534868de83891a15b917bad635308ef4482ec02
Davee <davidossoh@gmail.com>
2022-10-24 10:25:04 -0700

    Devops

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -3,6 +3,7 @@
 /*
  * _strlen - calculates string length
  * @s: assigned character
+ *
  * Return: returns the strlen of i
  **/
 
@@ -11,6 +12,6 @@ int _strlen(char *s)
 	int i;
 
 	for (i = 0; s[i] != '\0'; i++)
-	{}
+		{}
 	return(i);
 }
