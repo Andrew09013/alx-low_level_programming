567af7f95255d9488db728f95de645e3de533006
Andrew09013 <65587879+Andrew09013@users.noreply.github.com>
2022-12-06 00:46:37 +0530

    Devops

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -8,6 +8,7 @@
 #include <unistd.h>
 #include <string.h>
 
+int main(void);
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
