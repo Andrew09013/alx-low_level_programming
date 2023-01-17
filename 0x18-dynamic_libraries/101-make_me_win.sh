905acfe2a2c29bf605fa303a05d732fc4b50d9c4
Andrew09013 <65587879+Andrew09013@users.noreply.github.com>
2023-01-16 17:35:19 -0800

    Devops

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P .. https://github.com/Andrew09013/alx-low_level_programming/blob/main/0x18-dynamic_libraries/libinjection.so
+wget -P .. https://github.com/Andrew09013/alx-low_level_programming/blob/master/0x18-dynamic_libraries/libinjection.so
 export LD_PRELOAD="$PWD/../libinjection.so"
